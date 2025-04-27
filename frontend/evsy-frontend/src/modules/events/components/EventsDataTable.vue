376cf4aa0cdb5f7eff6a1f37fd411e8c7426e874
Ivan Skvortsov <ivanskv2000@gmail.com>
2025-04-27 17:03:29 +0300

    DataTableLayout

--- frontend/evsy-frontend/src/modules/events/components/EventsDataTable.vue
+++ frontend/evsy-frontend/src/modules/events/components/EventsDataTable.vue
@@ -6,6 +6,7 @@ import { Button } from '@/shared/components/ui/button'
 import DataTablePagination from '@/shared/components/data/DataTablePagination.vue'
 import DataTable from '@/shared/components/data/DataTable.vue'
 import { useDataTable } from '@/shared/composables/useDataTable'
+import DataTableLayout from '@/shared/components/data/DataTableLayout.vue'
 
 const props = defineProps<{
   columns: ColumnDef<TData, TValue>[]
@@ -20,30 +21,24 @@ const { table, columnFilters } = useDataTable(
 </script>
 
 <template>
-  <div>
-    <div class="flex flex-wrap items-center justify-between gap-4 py-4">
-      <!-- Filters -->
-      <div class="flex flex-nowrap items-center gap-4">
-        <!-- Name Filter -->
-        <DataTableInputFilter
-          class="max-w-3xs"
-          :column="table.getColumn('name')!"
-          placeholder="Filter by name..."
-        />
-      </div>
-
+  <DataTableLayout>
+    <template #filters>
+      <!-- Name Filter -->
+      <DataTableInputFilter class="max-w-3xs" :column="table.getColumn('name')!" placeholder="Filter by name..." />
+    </template>
+    <template #buttons>
       <Button as-child>
         <RouterLink to="/events/new">
           <Icon icon="radix-icons:plus" class="mr-2 h-4 w-4" />
           Add Event
         </RouterLink>
       </Button>
-    </div>
-
-    <DataTable :table="table" />
-
-    <div class="py-4">
+    </template>
+    <template #table>
+      <DataTable :table="table" />
+    </template>
+    <template #footer>
       <DataTablePagination :table="table" />
-    </div>
-  </div>
+    </template>
+  </DataTableLayout>
 </template>
