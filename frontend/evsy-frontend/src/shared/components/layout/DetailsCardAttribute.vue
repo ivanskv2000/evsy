5dca8cd2dedd1169d70a14f8dfd7968cb99fc4b4
Ivan Skvortsov <ivanskv2000@gmail.com>
2025-04-29 10:13:32 +0300

    Frontend improvements

---
+++ frontend/evsy-frontend/src/shared/components/layout/DetailsCardAttribute.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+
+const props = defineProps<{
+    icon: string
+    label: string
+    value?: string
+}>()
+</script>
+
+<template>
+    <div class="flex flex-wrap items-center gap-1">
+        <div class="flex items-center gap-1">
+            <Icon :icon="icon" class="h-4 w-4" />
+            <span>{{ label }}:</span>
+        </div>
+        <span v-if="value">{{ value }}</span>
+        <slot name="value" />
+    </div>
+</template>
