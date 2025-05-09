d4c7337cdea93180e45f1d90edb76c95d0d64ea3
root <ivanskv2000@gmail.com>
2025-05-09 09:00:07 +0000

    feat: top dropdown menu in the navbar

--- frontend/src/shared/components/layout/MainLayout.vue
+++ frontend/src/shared/components/layout/MainLayout.vue
@@ -8,6 +8,15 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from '@/shared/ui/navigation-menu'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/shared/ui/dropdown-menu'
 import ThemeProvider from '@/shared/components/providers/theme-provider.vue'
 </script>
 
@@ -57,6 +66,42 @@ import ThemeProvider from '@/shared/components/providers/theme-provider.vue'
               <span class="sr-only">Toggle theme</span>
             </Button>
           </ThemeProvider>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+              <Button variant="outline" size="icon">
+                <Icon icon="radix-icons:hamburger-menu" class="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent class="w-56">
+              <DropdownMenuGroup>
+                <ThemeProvider v-slot="{ toggleTheme }">
+                  <DropdownMenuItem @click="toggleTheme">
+                    <Icon
+                      icon="radix-icons:moon"
+                      class="h-4 w-4 scale-100 rotate-0 opacity-100 transition-all duration-300 ease-in-out dark:scale-0 dark:-rotate-90 dark:opacity-0"
+                    />
+                    <Icon
+                      icon="radix-icons:sun"
+                      class="absolute h-4 w-4 scale-0 rotate-90 opacity-0 transition-all duration-300 ease-in-out dark:scale-100 dark:rotate-0 dark:opacity-100"
+                    />
+                    <span class="sr-only">Toggle theme</span>
+                  </DropdownMenuItem>
+                </ThemeProvider>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuGroup>
+                <DropdownMenuItem>
+                  <Icon icon="simple-icons:github" class="mr-2 h-4 w-4" />
+                  <span>GitHub source</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Icon icon="simple-icons:buymeacoffee" class="mr-2 h-4 w-4" />
+                  <span>Buy me a coffee</span>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
     </header>
