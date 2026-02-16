093af2395df965c192684b23ebd96339d4039d5c
Ivan Skvortsov <ivanskv2000@gmail.com>
2026-02-16 19:35:13 +0100

    Tiny UI fix

--- frontend/src/modules/auth/components/LoginForm.vue
+++ frontend/src/modules/auth/components/LoginForm.vue
@@ -52,7 +52,7 @@ const onSubmit = handleSubmit(values => runLogin(values))
 
       <!-- Divider -->
       <div
-        v-if="!isDemo"
+        v-if="!isDemo && auth.availableProviders.length"
         class="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
       >
         <span class="bg-background text-muted-foreground relative z-10 px-2">
