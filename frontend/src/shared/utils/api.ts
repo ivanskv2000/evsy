a110526a5efafc4e8dc584a068e80159b36952a0
Ivan Skvortsov <ivanskv2000@gmail.com>
2026-02-23 13:57:58 +0100

    fix: tiny change in frontend api.ts

--- frontend/src/shared/utils/api.ts
+++ frontend/src/shared/utils/api.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from '@/modules/auth/stores/useAuthStore'
 import router from '@/router'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? '/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || '/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
