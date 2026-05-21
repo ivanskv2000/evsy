b2cb87778c4af83a9291d9447d66c5d693094946
Ivan Skvortsov <54892573+ivanskv2000@users.noreply.github.com>
2026-05-21 13:20:04 +0200

    feat(frontend): bundle fonts locally to comply with EU privacy regulations (#50)

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { installVueQuery } from './shared/plugins/vue-query'
 import router from './router'
+import '@fontsource-variable/inter'
+import '@fontsource/pacifico'
 import './index.css'
 import App from '@/App.vue'
 
